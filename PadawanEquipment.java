c1a240a935c0d0ef2391a8520d75e8d7fc06c324
danny981219 <122781492+danny981219@users.noreply.github.com>
2023-01-17 00:41:50 +0200

    latest
    last 2 of first week

---
+++ PadawanEquipment.java
@@ -0,0 +1,33 @@
+package JavaProgrammingFundamentals;
+
+import java.util.Scanner;
+
+public class PadawanEquipment {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double money = Double.parseDouble(scanner.nextLine());
+        int students = Integer.parseInt(scanner.nextLine());
+
+        double priceLightsabers = Double.parseDouble(scanner.nextLine());
+        double priceRobes = Double.parseDouble(scanner.nextLine());
+        double priceBelts = Double.parseDouble(scanner.nextLine());
+
+        double priceLightsabersClear = Math.ceil(students + 0.10 * students) * priceLightsabers;
+        double priceRobesFinal = students * priceRobes;
+
+        double sumBelts = (students - students / 6) * priceBelts;
+
+
+        double sum = priceLightsabersClear + priceRobesFinal + sumBelts;
+
+        if (money >= sum) {
+            System.out.printf("The money is enough - it would cost %.2flv.", sum);
+        } else {
+            System.out.printf("George Lucas will need %.2flv more.", sum - money);
+        }
+
+
+    }
+
+}
